7bedee1d7372d1ced3f4cffe3f30cd4670a33f69
AdrielPaivaFitcard <adriel.paiva@fitcard.com.br>
2023-10-11 23:36:26 -0300

    Adicionar arquivos de projeto.

---
+++ FI.AtividadeEntrevista/BLL/BoCliente.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoCliente
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Incluir(cliente);
+        }
+
+        /// <summary>
+        /// Altera um cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public void Alterar(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Alterar(cliente);
+        }
+
+        /// <summary>
+        /// Consulta o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public DML.Cliente Consultar(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Listar()
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Listar();
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Pesquisa(iniciarEm,  quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// VerificaExistencia
+        /// </summary>
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string cpf, long id = 0)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.VerificarExistencia(cpf, id);
+        }
+
+        /// <summary>
+        /// Remove a mascara do CPF
+        /// </summary>
+        public string RemoverMascaraCpf(string cpf)
+        {
+            string padrao = @"\D"; // \D corresponde a "não dígito"
+
+            return Regex.Replace(cpf, padrao, string.Empty);
+        }
+
+        // <summary>
+        /// Verificar se é um CPF válido
+        /// </summary>
+        public bool VerificarSeCpfValido(string cpf)
+        {
+            // Verifique se todos os dígitos são iguais; nesse caso, é inválido
+            //if (cpf.Distinct().Count() == 1)
+            //{
+            //    return false;
+            //}
+
+            // Verifique se todos os dígitos são iguais; nesse caso, é inválido
+            if (cpf.All(d => d == cpf[0]))
+            {
+                return false;
+            }
+
+            // Calcula o primeiro dígito verificador
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+            }
+            int resto = soma % 11;
+            int digitoVerificador1 = (resto < 2) ? 0 : 11 - resto;
+
+            // Verifique se o primeiro dígito verificador está correto
+            if (int.Parse(cpf[9].ToString()) != digitoVerificador1)
+            {
+                return false;
+            }
+
+            // Calcula o segundo dígito verificador
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+            }
+            resto = soma % 11;
+            int digitoVerificador2 = (resto < 2) ? 0 : 11 - resto;
+
+            // Verifique se o segundo dígito verificador está correto
+            if (int.Parse(cpf[10].ToString()) != digitoVerificador2)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -0,0 +1,188 @@
+﻿using FI.AtividadeEntrevista.BLL;
+using FI.AtividadeEntrevista.DML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebAtividadeEntrevista.Models;
+
+namespace WebAtividadeEntrevista.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Incluir(ClienteModel model)
+        {
+            try
+            {
+                BoCliente bo = new BoCliente();
+
+                if (!this.ModelState.IsValid)
+                {
+                    List<string> erros = (from item in ModelState.Values
+                                          from error in item.Errors
+                                          select error.ErrorMessage).ToList();
+
+                    Response.StatusCode = 400;
+                    return Json(string.Join(Environment.NewLine, erros));
+                }
+                else
+                {
+                    var cpf = bo.RemoverMascaraCpf(model.CPF);
+
+                    if (!bo.VerificarSeCpfValido(cpf))
+                    {
+                        Response.StatusCode = 400;
+                        return Json("Por favor, digite um CPF válido!");
+                    }
+
+                    if(bo.VerificarExistencia(cpf))
+                    {
+                        Response.StatusCode = 400;
+                        return Json("CPF já cadastrado!");
+                    }
+
+                    model.Id = bo.Incluir(new Cliente()
+                    {
+                        CEP = model.CEP,
+                        Cidade = model.Cidade,
+                        CPF = cpf,
+                        Email = model.Email,
+                        Estado = model.Estado,
+                        Logradouro = model.Logradouro,
+                        Nacionalidade = model.Nacionalidade,
+                        Nome = model.Nome,
+                        Sobrenome = model.Sobrenome,
+                        Telefone = model.Telefone
+                    });
+
+
+                    return Json("Cadastro efetuado com sucesso");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+            
+        }
+
+        [HttpPost]
+        public JsonResult Alterar(ClienteModel model)
+        {
+            BoCliente bo = new BoCliente();
+       
+            if (!this.ModelState.IsValid)
+            {
+                List<string> erros = (from item in ModelState.Values
+                                      from error in item.Errors
+                                      select error.ErrorMessage).ToList();
+
+                Response.StatusCode = 400;
+                return Json(string.Join(Environment.NewLine, erros));
+            }
+            else
+            {
+                var cpf = bo.RemoverMascaraCpf(model.CPF);
+
+                if (!bo.VerificarSeCpfValido(cpf))
+                {
+                    Response.StatusCode = 400;
+                    return Json("Por favor, digite um CPF válido!");
+                }
+
+                if (bo.VerificarExistencia(cpf, model.Id))
+                {
+                    Response.StatusCode = 400;
+                    return Json("CPF já cadastrado!");
+                }
+
+                bo.Alterar(new Cliente()
+                {
+                    Id = model.Id,
+                    CEP = model.CEP,
+                    Cidade = model.Cidade,
+                    CPF = cpf,
+                    Email = model.Email,
+                    Estado = model.Estado,
+                    Logradouro = model.Logradouro,
+                    Nacionalidade = model.Nacionalidade,
+                    Nome = model.Nome,
+                    Sobrenome = model.Sobrenome,
+                    Telefone = model.Telefone
+                });
+                               
+                return Json("Cadastro alterado com sucesso");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Alterar(long id)
+        {
+            BoCliente bo = new BoCliente();
+            Cliente cliente = bo.Consultar(id);
+            Models.ClienteModel model = null;
+
+            if (cliente != null)
+            {
+                model = new ClienteModel()
+                {
+                    Id = cliente.Id,
+                    CEP = cliente.CEP,
+                    Cidade = cliente.Cidade,
+                    CPF = cliente.CPF,
+                    Email = cliente.Email,
+                    Estado = cliente.Estado,
+                    Logradouro = cliente.Logradouro,
+                    Nacionalidade = cliente.Nacionalidade,
+                    Nome = cliente.Nome,
+                    Sobrenome = cliente.Sobrenome,
+                    Telefone = cliente.Telefone
+                };
+
+            
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult ClienteList(int jtStartIndex = 0, int jtPageSize = 0, string jtSorting = null)
+        {
+            try
+            {
+                int qtd = 0;
+                string campo = string.Empty;
+                string crescente = string.Empty;
+                string[] array = jtSorting.Split(' ');
+
+                if (array.Length > 0)
+                    campo = array[0];
+
+                if (array.Length > 1)
+                    crescente = array[1];
+
+                List<Cliente> clientes = new BoCliente().Pesquisa(jtStartIndex, jtPageSize, campo, crescente.Equals("ASC", StringComparison.InvariantCultureIgnoreCase), out qtd);
+
+                //Return result to jTable
+                return Json(new { Result = "OK", Records = clientes, TotalRecordCount = qtd });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+    }
+}
